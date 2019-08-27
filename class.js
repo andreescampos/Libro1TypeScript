67aa6f27add1f03a9976cfa87efea8584b9fe603
Andres Campos <andreescampos@hotmail.com>
2019-08-27 07:36:03 -0600

    Clases con Modificadores de Accesibilidad 2

--- class.js
+++ class.js
@@ -1,8 +1,45 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Shape = /** @class */ (function () {
     function Shape(name, width, height) {
+        this.name = name;
         this.area = width * height;
-        this.color = "pink";
+        this.color = "rosa";
     }
+    ;
+    Shape.prototype.shoutout = function () {
+    };
     ;
     return Shape;
 }());
+var Shape3D = /** @class */ (function (_super) {
+    __extends(Shape3D, _super);
+    function Shape3D(name, width, height, length) {
+        var _this = _super.call(this, name, width, height) || this;
+        _this.name = name;
+        _this.volume = length * _this.area;
+        return _this;
+    }
+    ;
+    Shape3D.prototype.shoutout = function () {
+        return "Soy un " + this.name + " con un volumen de " + this.volume + "cm cuadrados.";
+    };
+    Shape3D.prototype.superShout = function () {
+        return _super.prototype.shoutout.call(this);
+    };
+    return Shape3D;
+}(Shape));
+var cube = new Shape3D("cube", 30, 30, 30);
+console.log(cube.shoutout());
+console.log(cube.superShout());
