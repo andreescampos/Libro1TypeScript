dfba57c811038cdf6d59d4df5f65e87995402cfc
andres <andreescampos@hotmail.com>
2019-08-27 11:40:17 -0600

    Creando nuestra primera interfaz

--- interface.ts
+++ interface.ts
@@ -2,7 +2,7 @@ interface Shape {
     name:string;
     width: number;
     height:number;
-    color?:string;
+    color1?:string;
 }
 
 function area(shape:Shape){
@@ -12,4 +12,4 @@ function area(shape:Shape){
 
 
 console.log(area({name:"rectangulo",width: 30, height: 15}))
-console.log(area({name:"cuadrado",width: 30, height: 30, color:"blue"}));
+console.log(area({name:"cuadrado",width: 30, height: 30, color1:"blue"}));
