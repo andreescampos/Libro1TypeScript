476e62e9f42a5312714f56861b26e3358d6615f7
Andres Campos <andreescampos@hotmail.com>
2019-08-27 07:35:25 -0600

    Clases con Modificadores de Accesibilidad

---
+++ class.ts
@@ -0,0 +1,35 @@
+class Shape {
+    area: number;
+    private color: string;
+
+    constructor(public name: string, width: number, height: number) {
+        this.area = width * height;
+        this.color = "rosa";
+    };
+
+    shoutout() {
+
+    };
+
+}
+
+
+class Shape3D extends Shape {
+    volume: number;
+    constructor(public name: string, width: number, height: number, length: number) {
+        super(name, width, height);
+        this.volume = length * this.area;
+    };
+    shoutout() {
+        return "Soy un " + this.name + " con un volumen de " + this.volume + "cm cuadrados.";
+    }
+    superShout() {
+        return super.shoutout();
+    }
+}
+var cube = new Shape3D("cube", 30, 30, 30);
+console.log(cube.shoutout());
+console.log(cube.superShout());
+
+
+
