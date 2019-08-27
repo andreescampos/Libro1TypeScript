53e820a753d84eb6711c49542edd9472eb066576
andres <andreescampos@hotmail.com>
2019-08-27 15:42:15 -0600

    Herencia en Typescript

--- class2.ts
+++ class2.ts
@@ -16,3 +16,21 @@ personA.introduceSelf();
 personA.nombre = "Mindy";
  
 personA.introduceSelf();
+
+
+class Amigo extends Persona {
+    anios_conocido: number;
+    constructor(nombre: string, anios_conocido: number) {
+        super(nombre);
+        this.anios_conocido = anios_conocido;
+    }
+    timeKnown() {
+        console.log("Hemos sido amigos por " + this.anios_conocido + " años.")
+    }
+}
+ 
+let friendA = new Amigo("Jacob", 6);
+ 
+friendA.introduceSelf();
+ 
+friendA.timeKnown();
